7f15b9001f064a63ee8ca892f0fcbafd2317a9ad
szhh_wh <3116737282@qq.com>
2023-05-20 15:51:28 +0800

    Add Animation; Add some css

--- src/components/Randomapp.vue
+++ src/components/Randomapp.vue
@@ -3,7 +3,9 @@ import { invoke } from '@tauri-apps/api/tauri';
 //bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import { ref, watch } from 'vue';
+import { reactive, ref, watch } from 'vue';
+//Gsap Animation
+import { gsap } from 'gsap';
 
 const randnum = ref();
 const output = ref();
@@ -23,7 +25,7 @@ async function getnum() {
         for (var i = 0; i < times; i++) {
             do {
                 randnum.value = await invoke("generate_randnum", { min: min, max: max });
-            } while(record.includes(randnum.value as number))
+            } while (record.includes(randnum.value as number))
             record.push(randnum.value as number)
         }
     }
@@ -38,6 +40,16 @@ watch(randnum, () => {
     }
 })
 
+//Animation
+var show = ref(true);
+const tweened = reactive({
+    number: 0
+})
+
+watch(randnum, (n) => {
+    gsap.to(tweened, { duration: 0.5, number: Number(n) || 0 })
+})
+
 //init
 reset()
 </script>
@@ -45,8 +57,11 @@ reset()
 <template>
     <div class="main container-fluid">
         <div class="text-center row">
-            <p class="display-1" id="text">{{ randnum }}</p>
-            <p id="record" class="h4">{{ output }}</p>
+            <p class="display-1" id="text">{{ tweened.number.toFixed(0) }}</p>
+            <Transition>
+                <p v-if="show" class="h4">{{ output }}</p>
+            </Transition>
+            <!-- <p id="record" class="h4">{{ output }}</p> -->
         </div>
         <div class="num row">
             <div class="col-4">
@@ -65,9 +80,52 @@ reset()
         <div>
             <button @click="getnum()">抽取</button>
             <button @click="reset()">重置</button>
+            <button @click="show = !show">显示已抽取</button>
         </div>
     </div>
 </template>
 
 <style scoped>
+/* Animation */
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+
+/* Main css Start */
+.main {
+    height: 70vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.num {
+    margin: 20px 0;
+    display: flex;
+}
+
+input {
+    border-radius: 2px;
+}
+
+button {
+    width: 140px;
+    height: 50px;
+    border-radius: 5px;
+    background-color: white;
+    transition: background-color 0.3s;
+}
+
+button:hover {
+    background-color: rgba(138, 138, 138, 0.233);
+
+    transition: background-color 0.3s;
+}
 </style>
