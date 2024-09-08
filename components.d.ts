633f518b9fd6c06c85c06b59fd36862fb106d731
王浩 <3116737282@qq.com>
2024-09-08 21:01:16 +0800

    update dpes

--- components.d.ts
+++ components.d.ts
@@ -16,7 +16,6 @@ declare module 'vue' {
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
-    ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElRow: typeof import('element-plus/es')['ElRow']
